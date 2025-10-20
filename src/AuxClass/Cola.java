d4604aaadbf139d4e6aa7e843a2ddeaae8aea245
Marcobetancourt9 <marco.betancourt@correo.unimet.edu.ve>
2025-10-19 23:44:20 -0400

    Estructuras de Datos

---
+++ src/AuxClass/Cola.java
@@ -0,0 +1,145 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package AuxClass;
+
+/**
+ *
+ * @author chela
+ */
+public class Cola<T> {
+
+    private Nodo<T> head;
+    private Nodo<T> tail;
+    private int size;
+
+    public Cola() {
+        this.head = null;
+        this.tail = null;
+        this.size = 0;
+    }
+
+    public void vaciar() {
+        this.head = null;
+        this.tail = null;
+        this.size = 0;
+    }
+
+    public boolean isEmpty() {
+        return this.head == null;
+    }
+
+    public void empty() {
+        this.head = null;
+        this.tail = null;
+        this.size = 0;
+    }
+
+    public void encolar(T dato) {
+        Nodo<T> nuevo = new Nodo<>(dato);
+        System.out.println();
+        if (isEmpty()) {
+            this.head = nuevo;
+            this.tail = nuevo;
+        } else {
+            this.tail.setpNext(nuevo);
+            this.tail = nuevo;
+        }
+        this.size++;
+    }
+
+    public void desencolar() {
+        if (!isEmpty()) {
+            if (this.size == 1) {
+                empty();
+            } else {
+                this.head = this.head.getpNext();
+                this.size--;
+            }
+        }
+    }
+
+    public String travel() {
+        String toPrint = "";
+        if (!isEmpty()) {
+            Nodo<T> actual = this.head;
+            while (actual != null) {
+                toPrint += actual.gettInfo() + "-->";
+                actual = actual.getpNext();
+            }
+        }
+        return toPrint;
+    }
+
+    public List generarLista() {
+        List listaProcesos = new List("");
+        if (!isEmpty()) {
+            Nodo<T> actual = this.head;
+            while (actual != null) {
+                listaProcesos.append(actual);
+                actual = actual.getpNext();
+            }
+        }
+        return listaProcesos;
+    }
+
+    public void desencolarEspecifico(T dato) {
+        if (isEmpty()) {
+            System.out.println("La cola está vacía.");
+            return;
+        }
+
+        // Caso especial: si el nodo a eliminar es el head
+        if (this.head.gettInfo().equals(dato)) {
+            desencolar(); // Usa el método existente para desencolar el primer nodo
+            return;
+        }
+
+        Nodo<T> actual = this.head;
+        Nodo<T> anterior = null;
+
+        while (actual != null) {
+            if (actual.gettInfo().equals(dato)) {
+                // Nodo encontrado
+                if (anterior != null) {
+                    anterior.setpNext(actual.getpNext()); // Salta el nodo actual
+                    if (actual == this.tail) {
+                        this.tail = anterior; // Actualiza el tail si es necesario
+                    }
+                }
+                this.size--; // Disminuye el tamaño
+                System.out.println("Nodo con dato " + dato + " desencolado.");
+                return;
+            }
+            anterior = actual;
+            actual = actual.getpNext();
+        }
+
+        System.out.println("Nodo con dato " + dato + " no encontrado en la cola.");
+    }
+
+    public Nodo<T> getHead() {
+        return head;
+    }
+
+    public void setHead(Nodo<T> head) {
+        this.head = head;
+    }
+
+    public Nodo<T> getTail() {
+        return tail;
+    }
+
+    public void setTail(Nodo<T> tail) {
+        this.tail = tail;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+}
---
+++ src/AuxClass/List.java
@@ -0,0 +1,128 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package AuxClass;
+
+/**
+ *
+ * @author chela
+ */
+public class List <T>{
+    private Nodo <T> pFirst;
+    private String name;
+    private int iN;
+    
+    
+    public List (String s){
+        this.name=s;
+        this.pFirst=null;
+        this.iN=0;
+    }
+    
+    public Nodo<T> obtenerNodo(Object elem) {
+        Nodo<T> actual = this.pFirst;
+        while (actual != null) {
+            if (actual.gettInfo().equals(elem)) {
+                return actual;
+            }
+            actual = actual.getpNext();
+        }
+        return null;
+    }
+    
+    public int size (){
+        return this.iN;
+    }
+    
+    public boolean isEmpty(){
+        return this.pFirst==last();
+    }
+    
+    public Nodo last(){
+        return null;
+    }
+    
+    public Nodo first(){
+        return this.pFirst;
+    }
+    
+    public T read(Nodo <T> pValor){
+        return pValor.gettInfo();
+    }
+    
+    public Nodo <T> next (Nodo <T> pValor){
+        if (pValor != this.last()){
+            return pValor.getpNext();
+        }else{
+            return null;
+        }
+    }
+    
+    public T get(int index) {
+        if (index < 0 || index >= iN) {
+            throw new IndexOutOfBoundsException("index overflow");
+        }
+
+        Nodo<T> current = pFirst;
+        for (int i = 0; i < index; i++) {
+            current = current.getpNext();
+        }
+        return current.gettInfo();
+    }
+    
+    public void insert(T x, Nodo <T> pValor){
+     // postinsertar.   
+        Nodo <T> pNew= new Nodo <>(x);
+        if (this.isEmpty()){
+            this.pFirst=pNew;
+        }else{
+            pNew.setpNext(pValor.getpNext());
+            pValor.setpNext(pNew);
+        }
+        iN++;
+    }
+    public String travel (){
+        Nodo <T> pAux; 
+        String result="";
+        if(this.isEmpty()){
+            result="Está vacía";
+        }else{
+            pAux=this.first();
+            while (pAux != this.last()){
+                result=result+this.read(pAux)+ ", ";
+                pAux=this.next(pAux);
+            }
+            
+        }
+        return result;
+    }
+    
+    public void append(T data) {
+        Nodo<T> newNode = new Nodo<>(data);
+        if (pFirst == null) {
+            pFirst = newNode;
+        } else {
+            Nodo<T> temp = pFirst;
+            while (temp.getpNext()!= null) {
+                temp = temp.getpNext();
+            }
+            temp.setpNext(newNode);
+        }
+        iN++;
+    }
+    
+    
+    public Nodo <T> beforeLast(){
+        if (this.isEmpty()){
+            return null;
+        }else{
+            Nodo <T> pAux=this.first();
+            while (pAux.getpNext()!= this.last()){
+                pAux=this.next(pAux);
+            }
+            return pAux;
+        }
+    }
+}
+
