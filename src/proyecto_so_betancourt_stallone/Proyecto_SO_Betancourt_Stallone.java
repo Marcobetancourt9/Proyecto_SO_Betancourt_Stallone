e968f7ab5b96137cb5ac7d1b9a3d3042a6d09411
Marcobetancourt9 <marco.betancourt@correo.unimet.edu.ve>
2025-10-19 23:31:55 -0400

    Primer commit

---
+++ src/proyecto_so_betancourt_stallone/Proyecto_SO_Betancourt_Stallone.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyecto_so_betancourt_stallone;
+
+/**
+ *
+ * @author marco
+ */
+public class Proyecto_SO_Betancourt_Stallone {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
